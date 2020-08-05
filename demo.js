7d0faf1db4577bf3573f0b7bd82f4596ce8a24da
Pavani Nemuri <pavaninemuri8@gmail.com>
2020-08-05 11:53:20 +0530

    .

--- demo.js
+++ demo.js
@@ -1,1 +1,16 @@
-console.log("Hello world!!!!!/.............Feature branch")
+// console.log("Hello world!!!!!/.............Feature branch")
+const exp = "2m010213520019";
+const exp1 = "20\d{8}019";
+const var1 = '20';
+const var2 = '019';
+// const regex = new RegExp(var1 + "\\d{8}" + var2);
+const regex = new RegExp("^[0-9]*$")
+console.log(regex.test(exp));
+
+// const str = "2571857291729";
+// console.log(str.substring(2,5));
+
+// const date = new Date();
+// console.log(date);
+// console.log(date.getTime());
+// console.log(date.getUTCDate());
